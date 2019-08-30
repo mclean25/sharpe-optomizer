ea77b178bde8eea51dc5625ebbf68ce24af231d6
Alex McLean <alex.mclean25@gmail.com>
2019-08-29 22:25:52 -0700

    adding tqdm

--- data_manager.py
+++ data_manager.py
@@ -1,6 +1,8 @@
 import csv
 import pandas_datareader as web
 
+from tqdm import tqdm
+
 from preferences import Preferences
 from models.financial_instruments import Stock
 
@@ -17,27 +19,37 @@ class DataManager(object):
         """
         
         working_stocks = []
-        print("Collecting bulk data...")
 
-        with open(csv_path, 'r', encoding='utf-8') as csvfile:
-            all_symbols = csv.reader(csvfile)
-            for symbol in all_symbols:
-                ticker = symbol[0]
-                try:
-                    data = web.DataReader(
-                        name=ticker,
-                        data_source='yahoo',
-                        start=Preferences.BEGDATE,
-                        end=Preferences.ENDDATE)
-                except:
-                    print("Couldn't find the ticker {0}".format(ticker))
-                    continue
-                else:
-                    working_stocks.append(Stock(ticker, data))
+        tickers_to_fetch = self._load_tickers_from_csv(csv_path)
 
-                    print("successfully loaded {0}".format(ticker))
+        for ticker in tqdm(tickers_to_fetch):
+            try:
+                data = web.DataReader(
+                    name=ticker,
+                    data_source='yahoo',
+                    start=Preferences.BEGDATE,
+                    end=Preferences.ENDDATE)
+            except:
+                continue
+            else:
+                working_stocks.append(
+                    Stock(
+                        ticker,
+                        data))
 
         print("Finished collecting bulk data")
         print("Successfully loaded {0} stocks".format(len(working_stocks)))
 
         return working_stocks
+
+
+    def _load_tickers_from_csv(self, csv_path: str) -> list:
+        
+        tickers_to_fetch = []
+
+        with open(csv_path, 'r', encoding='utf-8') as csvfile:
+            all_symbols = csv.reader(csvfile)
+            for symbol in all_symbols:
+                tickers_to_fetch.append(symbol[0])
+
+        return tickers_to_fetch
